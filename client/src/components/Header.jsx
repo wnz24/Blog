d2e089844e64917466662b7b2fd75b578be37e6f
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-02-04 12:23:35 +0500

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
                     <FaMoon />
                 </Button>
                 <Link to="/sign-in">
-                    <Button gradientDuoTone='purpleToBlue'>
+                    <Button gradientDuoTone='purpleToBlue' outline>
                         SignIn
                     </Button>
                 </Link>
