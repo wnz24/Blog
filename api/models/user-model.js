91c648bbc548c7fd63ee59cee6a01842ebe89829
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-02-04 12:54:47 +0500

    create user model

---
+++ api/models/user-model.js
@@ -0,0 +1,24 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+    useraname: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+},
+    { timeStamp: true }
+)
+
+const User = mongoose.model('User', UserSchema);
+
+export default User
