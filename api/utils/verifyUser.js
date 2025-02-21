713b845c8f24e7676bdecf03efab79b1b9adfeaa
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-02-21 22:42:13 +0500

    Add create post api route

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,20 +1,20 @@
 import jwt from "jsonwebtoken";
-import {errorHandler} from "./error.js";
+import { errorHandler } from "./error.js";
 
-export const verifyUser = (req,res,next)=>{
+export const verifyUser = (req, res, next) => {
     const token = req.cookies.access_token;
-    if(!token){
-        return next(errorHandler('Unauthorized',401))
-    }   
-    try{
-        const user = jwt.verify(token,process.env.JWT_SECRET);
-        if(!user){
-            return next(errorHandler(401,'Unauthorized'))
-        }   
+    if (!token) {
+        return next(errorHandler('Unauthorized', 401))
+    }
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        if (!user) {
+            return next(errorHandler(401, 'Unauthorized'))
+        }
         req.user = user;
         next();
-    }catch(error){
-        return next(errorHandler(401,'Unauthorized'))
-    }   
+    } catch (error) {
+        return next(errorHandler(401, 'Unauthorized'))
+    }
 
 }
