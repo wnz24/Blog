c5cc9a6e75531a3b569286c2e163e9b093c1b172
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-02-21 15:51:15 +0500

    Add Admin functionality

--- api/models/user-model.js
+++ api/models/user-model.js
@@ -19,7 +19,11 @@ const UserSchema = new mongoose.Schema({
     profilePhoto:{
         type:String,
         default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    }
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
 },
     { timestamps: true }
 )
