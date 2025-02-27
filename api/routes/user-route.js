e1a72eccd947fd4496e0a80789e595f969dbf106
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-02-27 16:36:25 +0500

    create getUsers API route

--- api/routes/user-route.js
+++ api/routes/user-route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {uploadImage,updateUser,deleteUser,signout} from "../controllers/user-controller.js"
+import {uploadImage,updateUser,deleteUser,signout,getUsers} from "../controllers/user-controller.js"
 import {verifyUser} from "../utils/verifyUser.js"  
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/upload',uploadImage)
 router.put('/update/:userId',verifyUser,updateUser)
 router.delete('/delete/:userId',verifyUser,deleteUser)
 router.post('/signout',signout)
+router.get('/getusers',verifyUser,getUsers)
 
 export default router;
