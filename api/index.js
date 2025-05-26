eea56e7d840243ef12129d77ec23e2b4dcd86626
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-05-26 15:28:09 +0500

    Add pre installation for deployment

--- api/index.js
+++ api/index.js
@@ -6,12 +6,17 @@ import authRoutes from './routes/auth-route.js'
 import postRoutes from './routes/post-route.js'
 import cookieParser from "cookie-parser";  
 import CommentRoutes from "./routes/comment-route.js" 
+import path from "path"
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGOURL).then(() => {
     console.log("MongoDB is connected")
+}).catch((err)=> {
+    console.log(err);
 })
+const __dirname = path.resolve()
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +30,12 @@ app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', CommentRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.arguments('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err, req, res, next) => {
     console.error("Error:", err); // Debugging log
